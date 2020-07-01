899be0427ea34b0cac256ab544ec5e1345f58b6d
Matheus Voltane Mendonça <65687322+theusvoltane@users.noreply.github.com>
2020-07-01 13:31:02 -0300

    Update main.js
    Mais conseitos basicos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
+/*
 var nome = "Matheus Voltane Mendonça";
 var idade = 26;
 var idade2 = 10;
 var frase = "USA é o melhor time do mundo";
-//alert(nome + " tem " + idade + "anos");
-//alert(idade + idade2);
+alert(nome + " tem " + idade + "anos");
+alert(idade + idade2);
 console.log(nome);
 console.log(idade * idade2);
 console.log(frase.toUpperCase());
@@ -57,3 +58,59 @@ console.log(d.getYear());
 console.log(d.getHours());
 console.log(d.getMinutes());
 console.log(d.getSeconds());
+
+console.log("");
+function soma(n1,n2){
+	return n1+n2;
+}
+alert(soma(5,10));
+
+console.log("");
+function setReplece(frase,nome,novo_nome){
+	return frase.replace(nome, novo_nome);
+}
+
+alert(setReplece("Vai Argentina","Argentina","Brasil"));
+
+console.log("");
+function validaIdade(idade){
+	var validar;
+	if(idade>=18){
+		validar = true;
+	}else{
+		validar = false;
+	}
+	return validar;
+}
+
+var idade = prompt("Quantos anos você tem?");
+console.log(validaIdade(idade));
+*/
+
+function clicou(){
+	document.getElementById("agradecimento").innerHTML="<b>Obrigado Por Clicar<b>";
+	//alert("Obrigado por Clicar");
+}
+
+function redirecionar(){
+	window.open("https://web.digitalinnovation.one/home");
+	//window.location.href = "https://github.com/";
+}
+
+function trocar(element){
+	document.getElementById("mousemove").innerHTML = "Obrigado per passar o mouse";
+	//alert("trocar texto");
+}
+
+function voltar(element){
+	document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+	//alert("trocar texto");
+}
+
+function load(){
+	alert("pagina carregada");
+}
+
+function funcaoChange(element){
+	console.log(element.value);
+}
